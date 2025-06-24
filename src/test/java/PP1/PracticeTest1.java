1639e6ec95871a01cd045f7ccd0ee03505147e8c
HP <HP@DESKTOP-CBO20NK>
2025-06-24 20:10:14 +0530

    Jenkins Practice

---
+++ src/test/java/PP1/PracticeTest1.java
@@ -0,0 +1,24 @@
+package PP1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+
+
+public class PracticeTest1 {
+
+	@Test(groups="smoke")
+	
+	public void m1Test() {
+		System.out.println("m1 executed");
+		System.out.println("String");
+	}
+	
+	@Test(groups="regression")
+		
+		public void m2Test() {
+			System.out.println("m2 Executed");
+		}
+	}
+	
+
---
+++ src/test/java/PP2/PracticeTest2.java
@@ -0,0 +1,20 @@
+package PP2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class PracticeTest2 {
+
+	@Test(groups="regression")
+	
+	public void m1Test() {
+		System.out.println("m1 test is executed");
+		System.out.println("Rakshita");
+	}
+	
+	@Test(groups="smoke")
+	public void SmoTest() {
+		System.out.println("m1 test is executed");
+		System.out.println("Rakshita");
+	}
+}
